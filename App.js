44853b1e4b930f2f60fd4fb6f66491160d9e4271
Hafizh <hafizhcj@yahoo.com.sg>
2023-10-11 20:22:52 +0800

    First change

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { useState } from "react";
+import { Text, Pressable, StyleSheet, View, FlatList } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+
+function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" }
+  ]);
+  
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+  
+  
+  
+ 
+
+
+  
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`
+      }
+    ]);
+  }
+  
+  
+  
+  function resetColors() {
+    setColorArray ([]);
+  }
+  
+  
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    
+    <Pressable
+    style={{ height: 40, justifyContent: "center" }}
+    onPress={addColor}>
+    <Text style={{ color: "red" }}>Add colour</Text>
+    </Pressable>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+    <Pressable
+    style={{ height: 40, justifyContent: "center" }}
+    onPress={resetColors}>
+
+    <Text style={{ color: "red" }}>Reset</Text>
+
+
+    </Pressable>
+
+    
+    
+    
+    
+    <FlatList
+    style={{ width: "100%" }}
+    data={colorArray}
+    renderItem={renderItem}
+    keyExtractor={(item) => item.id}
+    />
+    </View>
+    );
+  }
+  
+
+  const Stack = createStackNavigator();
+  
+  export default function App() {
+    return (
+      <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Colour List" component={HomeScreen} />
+      </Stack.Navigator>
+      </NavigationContainer>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        
+        backgroundColor: "#fff",
+        alignItems: "center",
+      },
+      list: {
+        width: "100%"
+      }
+    });
+    
