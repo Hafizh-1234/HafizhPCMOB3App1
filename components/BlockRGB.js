83ca7a86751c3bb7e8d9e0a93badef2dd6b3a899
Hafizh <hafizhcj@yahoo.com.sg>
2023-10-12 12:26:10 +0800

    fourth change

--- components/BlockRGB.js
+++ components/BlockRGB.js
@@ -2,14 +2,16 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export default function BlockRGB(props) {
- return (
-   <View
-     style={{
-       backgroundColor: `rgb(${props.red}, ${props.green}, ${props.blue})`,
-       padding: 30,
-       width: "100%",
-     }}
-   ></View>
- );
-}
+  const randomHeight = Math.floor(Math.random() * 200) + 100; 
 
+  return (
+    <View
+      style={{
+        backgroundColor: `rgb(${props.red}, ${props.green}, ${props.blue})`,
+        padding: 30,
+        width: "100%",
+        height: props.height || randomHeight, 
+      }}
+    ></View>
+  );
+}
